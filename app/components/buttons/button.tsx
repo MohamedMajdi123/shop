9f3c52463620e210d01455fceb471d2127090075
MohamedMajdi123 <mhammmmmed0104@gmail.com>
2025-03-31 09:00:54 +0200

    new add

--- app/components/buttons/button.tsx
+++ app/components/buttons/button.tsx
@@ -8,17 +8,20 @@ function Button() {
     const Theme = useContext(TheemContext);
     const buttonRef = useRef<HTMLButtonElement>(null);
     useEffect(() => {
-        if(`${Theme[0]}` == 'dark') {
-            if(buttonRef.current){
-                buttonRef.current.classList.remove('light');
-                buttonRef.current.classList.add('dark');
-                console.log(Theme)
-            }
-        } else {
-            if(buttonRef.current) {
-                buttonRef.current.classList.remove('dark');
-                buttonRef.current.classList.add('light');
-                console.log(Theme)
+        if(Theme) {
+
+            if(`${Theme[0]}` == 'dark') {
+                if(buttonRef.current){
+                    buttonRef.current.classList.remove('light');
+                    buttonRef.current.classList.add('dark');
+                    console.log(Theme)
+                }
+            } else {
+                if(buttonRef.current) {
+                    buttonRef.current.classList.remove('dark');
+                    buttonRef.current.classList.add('light');
+                    console.log(Theme)
+                }
             }
         }
     }, [Theme])
